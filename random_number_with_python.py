27bbec31ec726897b428f46fa173b515cc2e59cc
Anton Berisha <43340489+islo123@users.noreply.github.com>
2023-03-28 18:23:44 +0300

    Add files via upload

---
+++ random_number_with_python.py
@@ -0,0 +1,33 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+        print("Please type a number larger than 0")
+        quit()
+else:
+    print("Please type a number next time")
+
+guess = 0;
+
+random_number = random.randint(0, top_of_range)
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+        guess += 1
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess < random_number:
+        print("You got it wrong the guess number is higher")
+    else:
+        print("You got it wrong guess number is lower")
+        
+print("You got it in", guess, "guesses")
